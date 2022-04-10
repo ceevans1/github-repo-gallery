c529b2c29c1c6b48f5a504e957e10a012b4a0d78
carl_e <aharah88@gmail.com>
2022-04-09 20:22:56 -0500

    Added repo list

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 //div with profile info
 const profileInfo = document.querySelector(".overview");
+//list of repos
+const repoList = document.querySelector(".repo-list");
 const username = "ceevans1"
 
 //fetch info from GitHub API
 const gatherInfo = async function () {
     const info = await fetch(`https://api.github.com/users/${username}`);
     const results = await info.json();
-    console.log(results)
+
 
     displayContent(results);
 }
@@ -25,6 +27,25 @@ const displayContent = function (data) {
         <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
     </div>`;
     profileInfo.append(displayDiv);
+    gatherRepos();
 }
+
+const gatherRepos = async function () {
+    const repos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=50`);
+    const repoArray = await repos.json();
+    displayRepos(repoArray);
+}
+
+const displayRepos = function (repos) {
+    repos.forEach(function (repo) {
+        const li = document.createElement("li");
+        li.classList.add("repo");
+        li.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(li);
+    })
+}
+
+
+
 
 gatherInfo();
